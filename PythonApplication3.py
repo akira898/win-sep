c9c3e03e967583e8b4e2429002f609c1701d3de8
akira898 <akiraproject984@gmail.com>
2026-09-25 08:18:44 +0200

    Add 'SEX' to forbiddenWords list

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -2,7 +2,7 @@ import keyboard
 from sonidosSeleccion import sound
 hystorial = []
 times = 0
-forbiddenWords = ["PORN", "FEMBOY","BOOPS","DICKS","COCKS"]
+forbiddenWords = ["PORN", "FEMBOY","BOOPS","DICKS","COCKS","SEX"]
 game=["dont","touch","keep","strong"]
 def al_presionar(key):
     global times, hystorial,game
